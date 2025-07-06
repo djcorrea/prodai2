19b54c45dbf8d0377d0c81c1a751ef963191a434
djcorrea <djcorreamg@gmail.com>
2025-07-06 20:09:48 -0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -3,6 +3,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+console.log('API KEY:', process.env.OPENAI_API_KEY);
 
   if (req.method === 'OPTIONS') return res.status(200).end();
 
